5f433a043768f6f5c8e98b4f16cb76698c903957
Akal-Ustat Singh <akalustat.singh@gmail.com>
2022-05-09 16:26:09 -0700

    attempt to fix workflow 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+   
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/akalustat.com/" : "",
 }
 
 module.exports = nextConfig
